a2357a51804017468537d81f920eea3f4429052a
Stephanie <seal12514@gmail.com>
2018-04-16 21:50:35 +0000

    Added popcode work, as well as adding some extra stuff!

--- js/script.js
+++ js/script.js
@@ -3,8 +3,30 @@
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 
 $(document).ready(function(){
+    function appendImageToBody(srcURL) {
+        $(".text-center").html("<img src=" + srcURL + ">");
+}
+
+    function callGiphyAPIWithSearchTerm(searchTerm) {
+        $.ajax({
+            url: "https://api.giphy.com/v1/stickers/search?q=" + searchTerm +"&api_key=dc6zaTOxFJmzC",
+            method: "GET",
+            success: function(response) {
+                var random = Math.floor(Math.random() * Math.floor(response.data.length));
+                var url = response.data[random].images.original.url;
+                appendImageToBody(url);
+      },
+    }); 
+}
+
+    $("button").click(function(){
+        var searchTerm = $("input").val();
+        callGiphyAPIWithSearchTerm(searchTerm);
+});
   
-  
-  
-  
+  $("#srch-term").keyup(function(event) {
+    if (event.keyCode === 13) {
+        $("#button").click();
+    }
+});
 });
